1cf024a44b0d337d67dc6fdc91c1b0c20a1411d1
keinsta <accounts@keinsta.com>
2025-06-30 21:55:04 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,6 +67,7 @@ export default function Navbar() {
               className="w-full text-left bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition"
               onClick={() => {
                 navigate("/");
+                setIsOpen(!isOpen);
               }}
             >
               Generate Bill
@@ -75,6 +76,7 @@ export default function Navbar() {
               className="w-full text-left bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700 transition"
               onClick={() => {
                 navigate("/find-customer-bill");
+                setIsOpen(!isOpen);
               }}
             >
               Find Bill
